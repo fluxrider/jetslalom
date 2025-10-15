f9c39a7cdd67f8a4b73290077e49b3ce42710c34
fluxrider <doompilot@gmail.com>
2025-10-15 14:21:43 -0400

    stub for compiling without gamepad support

---
+++ gamepad_none/Gamepad.java
@@ -0,0 +1,24 @@
+// This class is a stub for when you want to compile without gamepad support
+
+class Gamepad {
+
+  public boolean param_dpad_diag_count = true;
+
+  public boolean start;
+  public boolean south_maybe;
+  public boolean north_maybe;
+  public boolean west_maybe;
+  public boolean east_maybe;
+  public boolean up;
+  public boolean down;
+  public boolean left;
+  public boolean right;
+  public boolean left_shoulder;
+  public boolean right_shoulder;
+  public double left_trigger;
+  public double right_trigger;
+  public double lx, ly, rx, ry;
+
+  public void poll() { }
+
+}
