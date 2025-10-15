8925c8512c303b041af94b04667c0e6b5f7c0177
fluxrider <doompilot@gmail.com>
2025-10-15 17:58:09 -0400

    rewrite DPoint3 like a human would

--- DPoint3.java
+++ DPoint3.java
@@ -1,21 +1,6 @@
 public class DPoint3 {
-  double x;
-  
-  double y;
-  
-  double z;
-  
-  void setXYZ(double paramDouble1, double paramDouble2, double paramDouble3) {
-    this.x = paramDouble1;
-    this.y = paramDouble2;
-    this.z = paramDouble3;
-  }
-  
-  DPoint3() {}
-  
-  DPoint3(double paramDouble1, double paramDouble2, double paramDouble3) {
-    this.x = paramDouble1;
-    this.y = paramDouble2;
-    this.z = paramDouble3;
-  }
+  public double x, y, z;
+  public DPoint3() { }
+  public DPoint3(double x, double y, double z) { setXYZ(x,y,z); }
+  public void setXYZ(double x, double y, double z) { this.x = x; this.y = y; this.z = z; }
 }
