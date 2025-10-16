54759446f120ab8f1f9fb907a1726fe541292b1d
fluxrider <doompilot@gmail.com>
2025-10-16 14:18:10 -0400

    Canvas already knows it's width/height, no need to store it

--- NumberLabel.java
+++ NumberLabel.java
@@ -3,33 +3,29 @@ import java.awt.*;
 public class NumberLabel extends Canvas {
 
   private char[] data = new char[] { '0', '0', '0', '0', '0', '0' };
-  private int width;
-  private int height;
 
   public void setNum(int value) {
     for(int i = data.length - 1; i >= 0; i--) {
       this.data[i] = (char)(value % 10 + '0');
       value /= 10;
     }
-    Graphics g = getGraphics();
-    g.clearRect(0, 0, this.width, this.height);
-    paint(g);
+    Graphics g = this.getGraphics();
+    g.clearRect(0, 0, this.getWidth(), this.getHeight());
+    this.paint(g);
   }
 
   public NumberLabel(int width, int height) {
-    this.width = width;
-    this.height = height;
-    setSize(width, height);
-    setFocusable(false);
+    this.setSize(width, height);
+    this.setFocusable(false);
   }
 
   public void paint(Graphics g) {
     g.setColor(Color.black);
-    g.drawChars(this.data, 0, this.data.length, 4, this.height);
+    g.drawChars(this.data, 0, this.data.length, 4, this.getHeight());
   }
 
   public Dimension getPreferredSize() {
-    return new Dimension(this.width, this.height);
+    return this.getSize();
   }
 
 }
