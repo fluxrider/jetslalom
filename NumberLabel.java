192184aa089d46a1005228dabdebb1739a9d44ab
fluxrider <doompilot@gmail.com>
2025-10-15 21:46:16 -0400

    humanize, remove unecessary data

--- NumberLabel.java
+++ NumberLabel.java
@@ -1,45 +1,41 @@
 import java.awt.*;
 
 public class NumberLabel extends Canvas {
-  char[] data = new char[6];
+
+  private char[] data = new char[6];
+  private int width;
+  private int height;
   
-  Graphics gra;
-  
-  int width;
-  
-  int height;
-  
-  public void setNum(int paramInt) {
+  public void setNum(int value) {
     byte b = 0;
     while (true) {
-      this.data[5 - b] = (char)(paramInt % 10 + 48);
-      paramInt /= 10;
+      this.data[5 - b] = (char)(value % 10 + 48);
+      value /= 10;
       if (++b >= 6) {
-        if (this.gra == null)
-          this.gra = getGraphics(); 
-        this.gra.clearRect(0, 0, this.width, this.height);
-        paint(this.gra);
+        Graphics g =  getGraphics(); 
+        g.clearRect(0, 0, this.width, this.height);
+        paint(g);
         return;
       } 
     } 
   }
   
-  public NumberLabel(int paramInt1, int paramInt2) {
+  public NumberLabel(int width, int height) {
     byte b = 0;
     while (true) {
       this.data[b] = '0';
       if (++b >= 6) {
-        this.width = paramInt1;
-        this.height = paramInt2;
-        setSize(paramInt1, paramInt2);
+        this.width = width;
+        this.height = height;
+        setSize(width, height);
         return;
       } 
     } 
   }
   
-  public void paint(Graphics paramGraphics) {
-    paramGraphics.setColor(Color.black);
-    paramGraphics.drawChars(this.data, 0, 6, 4, this.height);
+  public void paint(Graphics g) {
+    g.setColor(Color.black);
+    g.drawChars(this.data, 0, 6, 4, this.height);
   }
   
   public Dimension getPreferredSize() {
