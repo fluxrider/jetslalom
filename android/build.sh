5fd1c065e64461dd040b21948067cdb6d1af8b14
fluxrider <doompilot@gmail.com>
2025-10-19 14:02:26 -0400

    tinker with the new way to do icons, without this it gets sized in a tiny square inside a circle... wow, android could have handled it itself better

--- android/build.sh
+++ android/build.sh
@@ -2,21 +2,12 @@ set -e
 
 ./clean.sh
 
-# I dislike modern framework so much. I'm storing files where I want, the way I want, and create the idiotic requirements android wants on the fly. I ain't storing 5 icon flavors.
-# Of course this means you better be running this script in an environment similar to mine (e.g. image magick)
+# I dislike modern framework so much. I'm storing only the files I want, location, format, and create the idiotic requirements android wants on the fly. I ain't storing 5 icon flavors.
+# Of course this means you better be running this script in an environment similar to mine (e.g. image magick is installed)
 
-# TODO just include xxxhdpi and see if it works fine on my 2025 phone. The android doc about icon seems to have been written in 2015.
-
-mkdir app/src/main/res/mipmap-mdpi
-mkdir app/src/main/res/mipmap-hdpi
-mkdir app/src/main/res/mipmap-xhdpi
-mkdir app/src/main/res/mipmap-xxhdpi
 mkdir app/src/main/res/mipmap-xxxhdpi
-
-magick ../icon_square_512.png -resize 48x48\! app/src/main/res/mipmap-mdpi/app_icon.png
-magick ../icon_square_512.png -resize 72x72\! app/src/main/res/mipmap-hdpi/app_icon.png
-magick ../icon_square_512.png -resize 96x96\! app/src/main/res/mipmap-xhdpi/app_icon.png
-magick ../icon_square_512.png -resize 144x144\! app/src/main/res/mipmap-xxhdpi/app_icon.png
-magick ../icon_square_512.png -resize 192x192\! app/src/main/res/mipmap-xxxhdpi/app_icon.png
+#magick ../icon_square_512.png -resize 512x512\! app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png
+cp ../icon_square_512.png app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png
+magick app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png -colorspace Gray app/src/main/res/mipmap-xxxhdpi/app_icon_grayscale.png
 
 gradle --warning-mode all assembleDebug
--- android/clean.sh
+++ android/clean.sh
@@ -2,9 +2,6 @@ set -e
 
 rm -Rf build
 rm -Rf app/build
-
-rm -Rf app/src/main/res/mipmap-mdpi
-rm -Rf app/src/main/res/mipmap-hdpi
-rm -Rf app/src/main/res/mipmap-xhdpi
-rm -Rf app/src/main/res/mipmap-xxhdpi
 rm -Rf app/src/main/res/mipmap-xxxhdpi
+
+#rm -Rf app/src/main/res/mipmap-anydpi-v26
