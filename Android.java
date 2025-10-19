38a973025dfba75c812147d64d1d835b08f07dd9
fluxrider <doompilot@gmail.com>
2025-10-19 18:07:53 -0400

    android test: drawing

--- Android.java
+++ Android.java
@@ -1,14 +1,24 @@
-import android.app.*;
-import android.os.*;
+import android.app.*; import android.os.*; import android.view.*;
+import android.graphics.*;
 
 public class Android extends Activity {
+  protected void onCreate(Bundle state) { super.onCreate(state); this.setContentView(new View(this) {
 
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
+    private Paint p = new Paint();
 
-  protected void onResume () {
-    super.onResume();
-  }
+    protected void onDraw(Canvas canvas) {
+      canvas.drawColor(0xffaaaaaa);
+      p.setColor(0xffff0000); canvas.drawLine((float)(Math.random() * 100), (float)(Math.random() * 100), (float)(Math.random() * 200 + 100), (float)(Math.random() * 200 + 100), p);
+    }
 
+    public boolean onTouchEvent(MotionEvent e) {
+      switch(e.getAction()) {
+        case MotionEvent.ACTION_UP:
+          this.invalidate();
+          break;
+      }
+      return true;
+    }
+
+  });}
 }
