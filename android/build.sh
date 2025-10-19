0ea8708c11134ee2e5e901476dd5d0bfbf6edcb4
fluxrider <doompilot@gmail.com>
2025-10-19 15:04:56 -0400

    generate everything

--- android/build.sh
+++ android/build.sh
@@ -5,7 +5,7 @@ set -e
 # I dislike modern framework so much. I'm storing only the files I want, location, format, and all then create the idiotic requirements android wants on the fly. I ain't storing 5 icon flavors or one line files that mean nothing to me.
 # Of course this means you better be running this script in an environment similar to mine (e.g. image magick is installed)
 
-# boring files, enjoy your slop gradle
+# boring files for gradle
 [ ! -f local.properties ] && echo "ERROR: local.properties is missing. Create it with path to android SDK (e.g. sdk.dir=/home/flux/_/apps/android-sdk/)" && exit 1
 #echo "org.gradle.jvmargs=-Xmx1536m" > gradle.properties # I'm gonna go out on a limb here and comment this out. Works for me with what I imagine are sensible defaults.
 echo "include ':app'" > settings.gradle
@@ -25,10 +25,69 @@ allprojects {
 }
 EOF
 
-# icon madness (from dpi folder mess, to handling round icon like idiots, now android wants a so called adaptive icon, which ends up being a xml file that is identical for us all, great engineering bud)
-mkdir app/src/main/res/mipmap-xxxhdpi
+# android config, mostly boring but it does contain the package name and android version min/target
+mkdir app
+cat > app/build.gradle <<EOF
+apply plugin: 'com.android.application'
+android {
+  namespace = "fluxrider.jetslalom"
+  compileSdkVersion = "android-36.1"; defaultConfig { minSdkVersion = 36; targetSdkVersion = 36; versionCode = 1; versionName = "1.0"; }
+  buildTypes { release { minifyEnabled = false } }
+  lint { abortOnError = false }
+}
+EOF
+mkdir -p app/src/main
+cat > app/src/main/AndroidManifest.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android" android:versionCode="1" android:versionName="1.0" android:installLocation="auto">
+  <application android:label="@string/app_name" android:icon="@mipmap/app_icon" android:roundIcon="@mipmap/app_icon" android:allowBackup="true" android:theme="@android:style/Theme.NoTitleBar.Fullscreen" android:hardwareAccelerated="true">
+    <activity android:name="Android"
+      android:label="@string/app_name"
+      android:alwaysRetainTaskState="true"
+      android:launchMode="singleInstance"
+      android:screenOrientation="landscape"
+      android:configChanges="layoutDirection|locale|orientation|uiMode|screenLayout|screenSize|smallestScreenSize|keyboard|keyboardHidden|navigation"
+      android:preferMinimalPostProcessing="true"
+      android:exported="true">
+      <intent-filter>
+        <action android:name="android.intent.action.MAIN" />
+        <category android:name="android.intent.category.LAUNCHER" />
+      </intent-filter>
+    </activity>
+  </application>
+</manifest>
+EOF
+mkdir -p app/src/main/res/values
+cat > app/src/main/res/values/strings.xml <<EOF
+<resources>
+  <string name="app_name">Jet Slalom Resurrected</string>
+</resources>
+EOF
+cat > app/src/main/res/values/colors.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+  <color name="colorPrimary">#3F51B5</color>
+  <color name="colorPrimaryDark">#303F9F</color>
+  <color name="colorAccent">#FF4081</color>
+  <color name="app_icon_background">#f6d66c</color>
+</resources>
+EOF
+cat > app/src/main/res/values/styles.xml <<EOF
+<resources>
+  <style name="AppTheme" parent="android:Theme.Holo.Light.DarkActionBar"></style>
+</resources>
+EOF
+
+# src
+mkdir -p app/src/main/java/fluxrider.jetslalom/
+cp ../Android.java app/src/main/java/fluxrider.jetslalom/
+
+# icon
+mkdir -p app/src/main/res/mipmap-xxxhdpi
 cp ../icon_512.png app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png
-magick app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png -colorspace Gray app/src/main/res/mipmap-xxxhdpi/app_icon_grayscale.png
+
+# icon madness (from dpi folder mess, to handling round icon like idiots, now android wants a so called adaptive icon, which ends up being a xml file that is identical for us all, great engineering bud)
+# and why the monochrome version, can't grayscale yourself?
 mkdir app/src/main/res/mipmap-anydpi-v26
 cat > app/src/main/res/mipmap-anydpi-v26/app_icon.xml <<EOF
 <?xml version="1.0" encoding="utf-8"?>
@@ -38,5 +97,7 @@ cat > app/src/main/res/mipmap-anydpi-v26/app_icon.xml <<EOF
   <monochrome android:drawable="@mipmap/app_icon_grayscale"/>
 </adaptive-icon>
 EOF
+magick app/src/main/res/mipmap-xxxhdpi/app_icon_foreground.png -colorspace Gray app/src/main/res/mipmap-xxxhdpi/app_icon_grayscale.png
 
+# finally, build
 gradle --warning-mode all assembleDebug
--- android/clean.sh
+++ android/clean.sh
@@ -1,11 +1,7 @@
 set -e
 
-rm -Rf build
-rm -Rf app/build
-
+rm -Rf build/
+rm -Rf app/
 rm -f gradle.properties
 rm -f settings.gradle
 rm -f build.gradle
-
-rm -Rf app/src/main/res/mipmap-xxxhdpi
-rm -Rf app/src/main/res/mipmap-anydpi-v26
